17e56a0d72a62636e1ed9d649fcf91f0a797d2c4
yoferzhang <522376829@qq.com>
2018-12-14 22:52:57 +0800

    save按钮

--- FoodPin/YQRestaurant/ViewController/YQNewRestaurantViewController.swift
+++ FoodPin/YQRestaurant/ViewController/YQNewRestaurantViewController.swift
@@ -8,15 +8,25 @@
 
 import UIKit
 
-class YQNewRestaurantViewController: UIViewController, UITableViewDelegate, UITableViewDataSource, UITextFieldDelegate {
+class YQNewRestaurantViewController: UIViewController, UITableViewDelegate, UITableViewDataSource, UITextFieldDelegate, UIImagePickerControllerDelegate, UINavigationControllerDelegate {
 
+    var restaurant: Restaurant!
     var editTableView: UITableView!
+    var photoImageView: UIImageView!
     
     override func viewDidLoad() {
         super.viewDidLoad()
         
         view.backgroundColor = .white
         
+        configureNav()
+        
+        initData()
+        initViews()
+        // Do any additional setup after loading the view.
+    }
+    
+    func configureNav() {
         navigationItem.largeTitleDisplayMode = .always
         navigationItem.title = "New Restaurant"
         
@@ -26,10 +36,12 @@ class YQNewRestaurantViewController: UIViewController, UITableViewDelegate, UITa
             NSAttributedString.Key.foregroundColor: UIColor.orange, NSAttributedString.Key.font: UIFont.boldSystemFont(ofSize: 35.0)
         ]
         
-        
-        
-        initViews()
-        // Do any additional setup after loading the view.
+        let rightItem = UIBarButtonItem(image: UIImage(named: "save"), style: .plain, target: self, action: #selector(YQNewRestaurantViewController.onClickNavRightButton(recognizer:)))
+        self.navigationItem.rightBarButtonItem = rightItem
+    }
+    
+    func initData() {
+        restaurant = Restaurant()
     }
     
     func initViews() {
@@ -56,6 +68,7 @@ class YQNewRestaurantViewController: UIViewController, UITableViewDelegate, UITa
         case 0:
             let cell = tableView.dequeueReusableCell(withIdentifier: String(describing: YQRestaurantEditPhotoCell.self), for: indexPath) as! YQRestaurantEditPhotoCell
             cell.selectionStyle = .none
+            photoImageView = cell.photoImageView
             return cell
 
         case 1:
@@ -124,6 +137,7 @@ class YQNewRestaurantViewController: UIViewController, UITableViewDelegate, UITa
                     let imagePicker = UIImagePickerController()
                     imagePicker.allowsEditing = false
                     imagePicker.sourceType = .camera
+                    imagePicker.delegate = self
                     
                     self.present(imagePicker, animated: true, completion: nil)
                 }
@@ -134,6 +148,7 @@ class YQNewRestaurantViewController: UIViewController, UITableViewDelegate, UITa
                     let imagePicker = UIImagePickerController()
                     imagePicker.allowsEditing = false
                     imagePicker.sourceType = .photoLibrary
+                    imagePicker.delegate = self
                     
                     self.present(imagePicker, animated: true, completion: nil)
                 }
@@ -155,5 +170,70 @@ class YQNewRestaurantViewController: UIViewController, UITableViewDelegate, UITa
         
         return true
     }
+    
+//    func textField(_ textField: UITextField, shouldChangeCharactersIn range: NSRange, replacementString string: String) -> Bool {
+//        switch textField.tag {
+//        case 1:
+//            restaurant.name = string
+//        case 2:
+//            restaurant.type = string
+//        case 3:
+//            restaurant.location = string
+//        case 4:
+//            restaurant.phone = string
+//        case 5:
+//            restaurant.description = string
+//        default:
+//            fatalError("Failed to change restaurant info")
+//        }
+//        return true
+//    }
+    
+    func textFieldShouldEndEditing(_ textField: UITextField) -> Bool {
+        switch textField.tag {
+        case 1:
+            restaurant.name = textField.text ?? ""
+        case 2:
+            restaurant.type = textField.text ?? ""
+        case 3:
+            restaurant.location = textField.text ?? ""
+        case 4:
+            restaurant.phone = textField.text ?? ""
+        case 5:
+            restaurant.description = textField.text ?? ""
+        default:
+            fatalError("Failed to change restaurant info")
+        }
+        return true
+    }
+    
+    //MARK: - UIImagePickerControllerDelegate
+    func imagePickerController(_ picker: UIImagePickerController, didFinishPickingMediaWithInfo info: [UIImagePickerController.InfoKey : Any]) {
+        if let selectedImage = info[UIImagePickerController.InfoKey.originalImage] as? UIImage {
+            photoImageView?.image = selectedImage
+            photoImageView?.contentMode = .scaleAspectFill
+            photoImageView?.clipsToBounds = true
+        }
+        
+        dismiss(animated: true, completion: nil)
+    }
 
+    //MARK: - 点击事件
+    @objc func onClickNavRightButton(recognizer: UITapGestureRecognizer) {
+        if restaurant.name.count <= 0 || restaurant.type.count <= 0 || restaurant.location.count <= 0 || restaurant.phone.count <= 0 || restaurant.description.count <= 0 {
+            
+            let okAction = UIAlertAction(title: "OK", style: .cancel, handler: nil)
+            let alertViewController = UIAlertController(title: "Oops", message: "We can't proceed because ont of the fields is blank. Please note that all fields are required.", preferredStyle: .alert)
+            
+            alertViewController.addAction(okAction)
+            
+            present(alertViewController, animated: true, completion: nil)
+        } else {
+            dismiss(animated: true) {
+                if super.isKind(of: ViewController.self) {
+                    var vc = super as? ViewController
+                }
+            }
+        }
+    }
 }
